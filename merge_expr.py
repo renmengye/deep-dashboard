202b4acc241e18d83b0677eb472824ff9b22ca1a
Mengye Ren <mren@twitter.com>
2016-09-29 16:45:58 -0400

    max step

--- merge_expr.py
+++ merge_expr.py
@@ -25,6 +25,7 @@ def parse_args():
     parser.add_argument('--results', default=None, type=str)
     parser.add_argument('--input', default=None, type=str)
     parser.add_argument('--output', default=None, type=str)
+    parser.add_argument('--max_step', default=None, type=int)
     return parser.parse_args()
 
 
@@ -34,9 +35,10 @@ def get_inputs(inp_str):
     return exp_pair
 
 
-def merge(input_pairs, output_folder):
+def merge(input_pairs, output_folder, max_step=None):
     data = []
     filename_name_map = {}
+    filename_type_map = {}
     for ii in xrange(len(input_pairs)):
         data_ii = {}  # filename maps to data_dict
         name = input_pairs[ii][0]
@@ -47,26 +49,47 @@ def merge(input_pairs, output_folder):
             for line in catalog_file:
                 parts = line.strip('\n').split(',')
                 # Only merge CSV file.
-                if parts[1] != 'csv':
+                if parts[1] != 'csv' and parts[1] != 'histogram':
                     continue
                 if parts[0] not in filename_name_map:
                     filename_name_map[parts[0]] = parts[2]
+                if parts[0] not in filename_type_map:
+                    filename_type_map[parts[0]] = parts[1]
                 filename = os.path.join(folder, parts[0])
-                data_ = []
-                names_ = []
-                with open(filename, 'r') as data_file:
-                    for line2 in data_file:
-                        parts2 = line2.strip('\n').split(',')
-                        if len(data_) == 0:
-                            for pp in parts2:
-                                names_.append(pp)
-                                data_.append([])
-                        else:
-                            for jj, pp in enumerate(parts2):
-                                if jj == 0:
-                                    data_[jj].append(int(pp))
+
+                if parts[1] == 'csv':
+                    data_ = []
+                    names_ = []
+                    with open(filename, 'r') as data_file:
+                        for line2 in data_file:
+                            parts2 = line2.strip('\n').split(',')
+                            if len(data_) == 0:
+                                for pp in parts2:
+                                    names_.append(pp)
+                                    data_.append([])
+                            else:
+                                do_add = False
+                                if max_step is not None:
+                                    if int(parts2[0]) < max_step:
+                                        do_add = True
                                 else:
-                                    data_[jj].append(pp)
+                                    do_add = True
+                                if do_add:
+                                    for jj, pp in enumerate(parts2):
+                                        if jj == 0:
+                                            data_[jj].append(int(pp))
+                                        else:
+                                            data_[jj].append(pp)
+
+                elif parts[1] == 'histogram':
+                    data_ = []
+                    names_ = []
+                    with open(filename, 'r') as data_file:
+                        for line2 in data_file:
+                            parts2 = line2.strip('\n').split(',')
+                            names_.append(parts2[0])
+                            data_.append(parts2[1:])
+
                 data_dict = {}  # For each stats file: key: values.
                 for data_jj, name_jj in zip(data_, names_):
                     data_dict[name_jj] = data_jj
@@ -78,55 +101,76 @@ def merge(input_pairs, output_folder):
     for data_ii in data:
         for kk in data_ii.keys():
             if kk not in merge_data:
-                merge_data[kk] = []
+                if filename_type_map[kk] == 'csv':
+                    merge_data[kk] = []
+                elif filename_type_map[kk] == 'histogram':
+                    merge_data[kk] = {}
 
     for ii, data_ii in enumerate(data):  # Experiments.
         for kk in data_ii.keys():  # Filename.
-            for jj in xrange(len(data_ii[kk].values()[0])):  # Time series.
-                item = {}
-                for ll in data_ii[kk].keys():
-                    if ll != 'step' and ll != 'time':
-                        if len(data_ii[kk].keys()) == 3:
-                            ll2 = input_pairs[ii][0]
+
+            if filename_type_map[kk] == 'csv':
+                for jj in xrange(len(data_ii[kk].values()[0])):  # Time series.
+                    item = {}
+                    for ll in data_ii[kk].keys():
+                        if ll != 'step' and ll != 'time':
+                            if len(data_ii[kk].keys()) == 3:
+                                ll2 = input_pairs[ii][0]
+                            else:
+                                ll2 = input_pairs[ii][0] + ' ' + ll
                         else:
-                            ll2 = input_pairs[ii][0] + ' ' + ll
+                            ll2 = ll
+                        item[ll2] = data_ii[kk][ll][jj]
+                    merge_data[kk].append(item)
+
+            elif filename_type_map[kk] == 'histogram':
+                for jj in data_ii[kk].keys():
+                    if len(data_ii[kk].keys()) == 1:
+                        ll = input_pairs[ii][0]
                     else:
-                        ll2 = ll
-                    item[ll2] = data_ii[kk][ll][jj]
-                merge_data[kk].append(item)
+                        ll = input_pairs[ii][0] + ' ' + jj
+                    merge_data[kk][ll] = data_ii[kk][jj]
 
     for kk in merge_data.keys():
-        print(kk)
-        # print(merge_data[kk])
-        merge_data[kk] = sorted(merge_data[kk], key=lambda x: x['step'])
+        if filename_type_map[kk] == 'csv':
+            merge_data[kk] = sorted(merge_data[kk], key=lambda x: x['step'])
 
     merge_catalog = os.path.join(output_folder, 'catalog')
     with open(merge_catalog, 'w') as f:
         f.write('filename,type,name\n')
     for kk in merge_data.keys():
         filename = os.path.join(output_folder, kk)
-        all_keys = []
-        for jj in xrange(len(merge_data[kk])):
-            for ll in merge_data[kk][jj].keys():
-                if ll not in all_keys:
-                    all_keys.append(ll)
-        with open(filename, 'w') as f:
-            f.write('step,time')
-            for ll in all_keys:
-                if ll != 'step' and ll != 'time':
-                    f.write(',{}'.format(ll))
-            f.write('\n') 
+
+        if filename_type_map[kk] == 'csv':
+            all_keys = []
             for jj in xrange(len(merge_data[kk])):
-                ss = '{},{}'.format(merge_data[kk][jj]['step'],
-                                    merge_data[kk][jj]['time'])
+                for ll in merge_data[kk][jj].keys():
+                    if ll not in all_keys:
+                        all_keys.append(ll)
+            with open(filename, 'w') as f:
+                f.write('step,time')
                 for ll in all_keys:
                     if ll != 'step' and ll != 'time':
-                        ss += ','
-                        if ll in merge_data[kk][jj]:
-                            ss += '{}'.format(merge_data[kk][jj][ll])
-                f.write('{}\n'.format(ss))
+                        f.write(',{}'.format(ll))
+                f.write('\n')
+                for jj in xrange(len(merge_data[kk])):
+                    ss = '{},{}'.format(merge_data[kk][jj]['step'],
+                                        merge_data[kk][jj]['time'])
+                    for ll in all_keys:
+                        if ll != 'step' and ll != 'time':
+                            ss += ','
+                            if ll in merge_data[kk][jj]:
+                                ss += '{}'.format(merge_data[kk][jj][ll])
+                    f.write('{}\n'.format(ss))
+
+        elif filename_type_map[kk] == 'histogram':
+            with open(filename, 'w') as f:
+                for jj in sorted(merge_data[kk].keys()):
+                    f.write(jj + ',' + ','.join(merge_data[kk][jj]) + '\n')
+
         with open(os.path.join(output_folder, 'catalog'), 'a') as f:
-            f.write('{},csv,{}\n'.format(kk, filename_name_map[kk]))
+            f.write('{},{},{}\n'.format(
+                kk, filename_type_map[kk], filename_name_map[kk]))
 
 
 def main():
@@ -153,7 +197,7 @@ def main():
         else:
             print('Folder "{}" not removed.'.format(output_folder))
             return
-    merge(input_pairs, output_folder)
+    merge(input_pairs, output_folder, max_step=args.max_step)
     pass
 
 if __name__ == '__main__':
